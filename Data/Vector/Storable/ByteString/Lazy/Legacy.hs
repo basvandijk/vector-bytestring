0e4e4b07a9c7d6ae5298aeb8ccfa0a0f71b5f53a
Bas van Dijk <v.dijk.bas@gmail.com>
2011-10-12 14:12:03 +0200

    Re-add the legacy conversion functions

---
+++ Data/Vector/Storable/ByteString/Lazy/Legacy.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | Convert our lazy @'ByteString's@ to and from /legacy/ lazy
+-- @'Legacy.ByteString's@ (from the @bytestring@ package).
+module Data.Vector.Storable.ByteString.Lazy.Legacy where
+
+--------------------------------------------------------------------------------
+-- Imports
+--------------------------------------------------------------------------------
+
+-- from base:
+import Data.Function ( (.) )
+
+-- from bytestring:
+import qualified Data.ByteString.Lazy.Internal as Legacy 
+    ( ByteString(Empty, Chunk), foldrChunks )
+
+-- from vector-bytestring (this package):
+import Data.Vector.Storable.ByteString.Lazy.Internal 
+    ( ByteString(Empty, Chunk), foldrChunks )
+
+import qualified Data.Vector.Storable.ByteString.Legacy as VSB 
+    ( toLegacyByteString, fromLegacyByteString )
+
+--------------------------------------------------------------------------------
+-- Legacy conversions
+--------------------------------------------------------------------------------
+
+-- | O(1) Convert our lazy 'ByteString' to a /legacy/ lazy 'Legacy.ByteString'
+-- (from the @bytestring@ package).
+toLegacyByteString :: ByteString -> Legacy.ByteString
+toLegacyByteString = foldrChunks (Legacy.Chunk . VSB.toLegacyByteString) Legacy.Empty
+{-# INLINE toLegacyByteString #-}
+
+-- | O(1) Convert a /legacy/ lazy 'Legacy.ByteString' (from the @bytestring@ package)
+-- to our lazy 'ByteString'.
+fromLegacyByteString :: Legacy.ByteString -> ByteString
+fromLegacyByteString = Legacy.foldrChunks (Chunk . VSB.fromLegacyByteString) Empty
+{-# INLINE fromLegacyByteString #-}
--- README.markdown
+++ README.markdown
@@ -1,3 +1,1 @@
 ByteStrings as type synonyms of Storable Vectors of Word8s.
-
-(Use the [spool](http://hackage.haskell.org/package/spool) package to convert from and to legacy bytestrings.
